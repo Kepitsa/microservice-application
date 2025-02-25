28048d124d90c0015a52467dec6655c1b85c81f1
Kepitsa <ohuje3382@bk.ru>
2025-02-25 18:58:14 +0300

    Remove codezero-template-1.20.1 from repository and update .gitignore

--- codezero-template-1.20.1/src/main/java/net/ohuje/CodeZero/extensions/ILastKnownPos.java
+++
@@ -1,8 +0,0 @@
-package net.ohuje.CodeZero.extensions;
-
-import net.minecraft.util.math.Vec3d;
-
-public interface ILastKnownPos {
-    Vec3d getLastKnownPos();
-    void setLastKnownPos(Vec3d pos);
-}
